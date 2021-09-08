c12ff5c102f2874e52bd59bf3802bc73dc7db3e5
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-08 11:24:10 +0530

    Added JS for click handler for generate btn

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const COLORS = ["#D82E2F", "#FF6263", "#207398", "#22CB5C", "#F7CD2E"];
+
+const generateBtn = document.querySelector(".action__container--btn");
+const template = document.querySelector("#template");
+const colorContainer = document.querySelector(".colorp");
+
+generateBtn.addEventListener("click", (e) => {
+  updateColors();
+});
+
+function updateColors() {
+  colorContainer.innerHTML = "";
+  for (let i = 0; i < 5; i++) {
+    const templateClone = template.content.cloneNode(true);
+    const cardEl = templateClone.querySelector(".card__color");
+    const codeEl = templateClone.querySelector(".card__color--code");
+
+    cardEl.style.backgroundColor = COLORS[i];
+    codeEl.textContent = COLORS[i];
+
+    colorContainer.appendChild(templateClone);
+  }
+}
